3ebc29bc2852f90675493c32ac1536a8a8076ef3
Apprentice <apprentice@dbc14.local>
2014-02-03 14:01:46 -0800

    Release 1: add button, leave 10px behind

--- source/main.js
+++ source/main.js
@@ -1,14 +1,21 @@
 $(document).ready(function(){
-  $("nav").hide();
-  $(".close").hide();
-  $(".open").click(function(){
-    $("nav").toggle("slide");
-    $(".close").show();
-    $(".open").hide();
-  });
-  $(".close").click(function(){
-    $("nav").toggle("slide");
-    $(".open").show();
-    $(".close").hide();
-  });
+  $('.toggle').click(toggleNav)
 });
+
+var toggleNav = function(){
+  var nav = $('nav')
+  if(nav.width() == 150){
+    $('nav').animate({width: "-=130px"}, "slow", hideUl);
+  } else {
+    $('nav').animate({width: "+=130px"}, "slow", showUl);
+  };
+};
+
+
+var hideUl = function(){
+  $('nav ul').hide();
+};
+
+var showUl = function(){
+  $('nav ul').show();
+};
