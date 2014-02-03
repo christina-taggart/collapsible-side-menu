f682f2b605b72d593801712d4463894f862240ae
& <pair+lee-sh-roy.mh120888@devbootcamp.com>
2014-02-03 14:25:38 -0800

    Side bar appears/disappears on hover

--- source/main.js
+++ source/main.js
@@ -1,7 +1,25 @@
-$('#show_sidebar a').on('click', function() {$('nav').css('display', 'block');
-$('#show_sidebar').css('display', 'none');
-$('#hide_sidebar').css('display', 'block')});
+// $('#show_sidebar a').on('click', function() {$('nav').css('display', 'block');
+// $('#show_sidebar').css('display', 'none');
+// $('#hide_sidebar').css('display', 'block')});
 
-$('#hide_sidebar a').on('click', function() {$('nav').css('display', 'none');
-$('#hide_sidebar').css('display', 'none');
-$('#show_sidebar').css('display', 'block')});
+// $('#hide_sidebar a').on('click', function() {$('nav').css('display', 'none');
+// $('#hide_sidebar').css('display', 'none');
+// $('#show_sidebar').css('display', 'block')});
+
+$('#show_sidebar a').hover(
+  function() {
+    $('nav').css('display', 'block');
+  },
+  function() {
+    $('nav').css('display', 'block');
+  }
+);
+
+$('nav').hover(
+  function() {
+    $('nav').css('display', 'block');
+  },
+  function() {
+    $('nav').css('display', 'none');
+  }
+);
