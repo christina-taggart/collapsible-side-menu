a0fb227275cac1c618816841525e55e613053e2e
shkurkin <eshkurkin@gmail.com>
2014-02-03 15:07:58 -0800

    added JQUERY funciton for sliding side bar menu up and down.

--- source/application.js
+++ source/application.js
@@ -1,3 +1,8 @@
-$(".toggle").click(function() {
-  $( "nav ul" ).slideUp();
+$( document ).ready(function() {
+  $(".toggle").click(function() {
+    $( "nav ul" ).slideToggle("slow")
+    $(this).html($(this).html() == 'Open Sesame &gt;&gt;&gt;' ? 'Close Sesame <<<' : 'Open Sesame >>>');
+  });
 });
+
+
