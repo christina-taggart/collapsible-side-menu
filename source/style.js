4b6a3fbe6b84037f33e917bda987cb6f3bd9658b
Apprentice <apprentice@dbc09.local>
2014-02-03 13:35:03 -0800

    Made sticky navbar collapsible.

---
+++ source/style.js
@@ -0,0 +1,18 @@
+// asdfasdfasdfasdf
+$( document ).ready(function() {
+    $('.nav-button').on("click", function() {
+      $('.nav').animate( {
+        width: "toggle"
+       });
+    });
+});
+
+// $( "nav" ).click(function() {
+//   $( "nav" ).animate({
+//     opacity: 0.25,
+//     left: "+=50",
+//     height: "toggle"
+//   }, 500, function() {
+//     // Animation complete.
+//   });
+// });
