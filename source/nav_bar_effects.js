e400cfec72b7ad0ffa987b4c06ff65fae5f700ad
Alexander Leishman & John Olmsted <pair+leishman.qsymmachus@devbootcamp.com>
2014-02-03 12:34:34 -0800

    Adds jquery functions for sliding navbar

--- source/nav_bar_effects.js
+++ source/nav_bar_effects.js
@@ -1,4 +1,18 @@
 // Shorthand for $( document ).ready()
 $(function() {
 
+  // Nav moves 120 pixels right on mouseover
+  $('nav').on("mouseenter",
+    function () {
+      $(this).animate({left: "+=120"}, 300)
+    }
+  )
+
+  // Nav moves 120 pixels left on mouseoff
+  $('nav').on("mouseleave",
+    function () {
+      $(this).animate({left: "-=120"}, 300)
+    }
+  )
+
 });
