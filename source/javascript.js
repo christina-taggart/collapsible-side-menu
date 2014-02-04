58f92f1a6ffdcb36f954b1bf1b694896877251fe
Patrick Vilhena <null>
2014-02-03 16:42:37 -0800

    Release 0: Hover slide and some prettiness

--- source/javascript.js
+++ source/javascript.js
@@ -1,5 +1,55 @@
-// $('nav').css('display', 'none')
+// $('#hide').on('click',
 
-$('#hide').on('click', function() {
-    $('nav').hide()
-    });
+
+
+// function navHide() {
+//     if ($('nav').css('display') != 'none'){
+//         $('nav').css('display', 'none');
+//         $('.container').css('padding-left', '20px');
+//         $('#hidelink').html('Open Sesame >>');
+//       }
+//       else
+//       {
+//          $('nav').css('display', 'show');
+//          $('.container').css('padding-left', '170px');
+//          $('#hidelink').html('Close Sesame <<');
+//       }
+//     }
+$(document).ready(function(){
+    $('nav').hover(
+      function(){
+        if (hoverOn) {
+          $(this).animate({left: '0px'});
+        }
+      },
+      function(){
+        if (hoverOn) {
+          $(this).animate({left: '-130px'});
+      }
+      }
+    )
+  }
+  )
+
+var hoverOn = false;
+
+function navHide() {
+  if ($('nav').css('left') == '0px'){
+    hoverOn = true;
+    $('nav').css('left', '-130px');
+    $('.container').css('padding-left', '20px');
+    $('#hidelink').html('Open Sesame &gt;&gt;&gt;');
+  }
+  else
+  {
+    hoverOn = false;
+   $('nav').css('left', '0px');
+   $('.container').css('padding-left', '170px');
+   $('#hidelink').html('Close Sesame &lt;&lt;&lt;');
+ }
+};
+
+
+
+
+
