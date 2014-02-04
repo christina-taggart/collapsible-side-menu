e95763487728fea6f8b26fe4989b1423460a2b71
Christina Taggart <ctaggart@Christinas-MacBook-Pro-2.local>
2014-02-03 16:56:49 -0800

    Release 1: Added double click nav hiding functionality

--- source/application.js
+++ source/application.js
@@ -6,28 +6,40 @@ $(function() {
 
 
 $(function() {
-  $( "#show").click(function() {
+  $( "#show").dblclick(function() {
     $("nav").toggle(
       $( "nav" ).animate({
-          width: "20px"
+        width: "20px"
 
-        }, 5000, function() {
-          $("ul").hide();
+      }, 5000, function() {
+        $("ul").hide();
 
-        })
+      })
       );
   });
 });
 
 
 $(function() {
-  $("#show").on("click", function() {
-      if ($(this).text() == "Open Sesame")
-         $(this).text("Close Sesame")
-      else
-         $(this).text("Open Sesame");
+  $( "ul").click(function() {
+      $( "nav" ).animate({
+        width: "150px"
+
+      }, 5000, function() {
+          // $("ul").hide();
+      })
   });
 });
+
+
+// $(function() {
+//   $("#show").on("click", function() {
+//       if ($(this).text() == "Open Sesame")
+//          $(this).text("Close Sesame")
+//       else
+//          $(this).text("Open Sesame");
+//   });
+// });
 
 
 
